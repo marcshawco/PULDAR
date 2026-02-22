6a758256d7173e48eac5b2acf03f55af43961275
Marc Shaw <marcus@marcuslshaw.com>
2026-02-22 02:36:35 -0800

    Initial Commit

---
+++ PULDAR/PULDARApp.swift
@@ -0,0 +1,17 @@
+//
+//  PULDARApp.swift
+//  PULDAR
+//
+//  Created by Marcus Shaw II on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PULDARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
